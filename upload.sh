573bffb02e0f12326d55bf8a5def31cc7faa9b7c
root <root@raspberrypi.(none)>
2014-08-11 14:35:40 +0200

    added readme

--- upload.sh
+++ upload.sh
@@ -1,32 +1,7 @@
-#!/bin/bash
-### BEGIN INIT INFO
-# Provides:          upload.sh
-# Required-Start:    $remote_fs $syslog $local_fs $network $time
-# Required-Stop:     $remote_fs $syslog
-# Default-Start:     2 3 4 5
-# Default-Stop:      0 1 6
-# Short-Description: Start daemon at boot time
-# Description:       Enable service provided by daemon.
-### END INIT INFO
-
- 
-case "$1" in
-	start)
-		echo "Starting example"
-		# run application you want to start
-		cd /home/
-		sudo python data_log.py &
-		sudo ./drop.sh &
-	;;
-	stop)
-		echo "Stopping example"
-		# kill application you want to stop
-		killall python
-	;;	
-	*)
-	echo "Usage: /etc/init.d/example{start|stop}"
-    exit 1
-    ;;
-esac
- 
-exit 0
+while true 
+do 
+	sudo ./dropbox_uploader.sh delete /data/
+	sudo ./dropbox_uploader.sh upload /home/data/ / 
+	sudo ./dropbox_uploader.sh download /control/logging_increment.txt /home/control/logging_increment.txt 
+	sleep 300 
+done
